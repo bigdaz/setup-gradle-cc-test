310287f5d1bb969aeb0ebcee2a330c229045f754
Daz DeBoer <daz@gradle.com>
2026-06-14 14:26:33 -0600

    Bump DV plugin version

--- no-build-logic/settings.gradle.kts
+++ no-build-logic/settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("com.gradle.develocity") version "4.2.1"
+    id("com.gradle.develocity") version "4.4.2"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
 }
 
